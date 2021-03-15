0f93af30513b7df186ef66820158c3c38bbc0fb3
MHammadYou <MHammadY2.0@gmail.com>
2021-03-15 23:40:39 +0500

    Player will fly away in the oposite direction

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float releaseTime;
+    
+    
+    private bool _isPressed;
+    void Update()
+    {
+        if (_isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        _isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    void OnMouseUp()
+    {
+        _isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+    }
+    
+}

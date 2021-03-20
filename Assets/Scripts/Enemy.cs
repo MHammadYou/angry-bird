4c86b7dc3ee7a70d1db17162bb05a1f0d6b61649
MHammadYou <MHammadY2.0@gmail.com>
2021-03-20 23:52:33 +0500

    Added enemies. Enemies will die if velocity is greater than 4f

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float health = 4f;
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.relativeVelocity.magnitude > health)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
